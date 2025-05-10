5e6b299501e0d2dfb4a9dc860db270a9baa35561
madn6 <msoffl0404@gmail.com>
2025-05-10 13:54:26 +0530

    feat: update TodayEntryForm styles and structure; replace icons, adjust labels, and enhance layout for better user experience

--- frontend/src/components/dashboard/entryForm/TodayEntryForm.tsx
+++ frontend/src/components/dashboard/entryForm/TodayEntryForm.tsx
@@ -1,5 +1,6 @@
 import React, { useCallback, useState } from 'react';
-import { Check, X, Send, AlertTriangle } from 'lucide-react';
+import { Check,Send, AlertTriangle } from 'lucide-react';
+import { PiCloverFill } from 'react-icons/pi';
 
 const TodayEntryForm: React.FC = () => {
 	const [type, setType] = useState<'lucky' | 'unlucky' | null>(null);
@@ -42,12 +43,12 @@ const TodayEntryForm: React.FC = () => {
 
 	return (
 		<div className="bg-secondary font-inter backdrop-blur-md rounded-xl p-6 border border-border/20">
-			<h2 className="text-xl font-semibold text-white mb-6">Today's Entry</h2>
+			<h2 className="text-xl font-semibold text-white text-center  mb-6">Today's Entry</h2>
 
 			<form onSubmit={handleSubmit}>
 				{/* Type Selector */}
 				<div className="mb-6">
-					<label className="block text-white text-sm font-medium mb-2">How was your day?</label>
+					<label className="block text-gray-text   font-medium mb-2">How was your day?</label>
 					<div className="flex space-x-4">
 						<button
 							type="button"
@@ -55,10 +56,10 @@ const TodayEntryForm: React.FC = () => {
 							className={`flex-1 py-3 px-4 rounded-lg flex items-center justify-center transition-all duration-200 ${
 								type === 'lucky'
 									? 'bg-green-500/20 border border-green-500 text-green-200'
-									: 'bg-gray border border-border/20 text-gray-300 '
+									: 'bg-gray border border-border/20 text-gray-text '
 							}`}
 						>
-							<Check className="mr-2" size={18} />
+							<PiCloverFill className="mr-1" size={18} />
 							Lucky
 						</button>
 						<button
@@ -67,10 +68,19 @@ const TodayEntryForm: React.FC = () => {
 							className={`flex-1 py-3 px-4 rounded-lg flex items-center justify-center transition-all duration-200 ${
 								type === 'unlucky'
 									? ' bg-orange-500/20 border border-orange-500 text-orange-200'
-									: 'bg-gray border border-border/20 text-gray-300 '
+									: 'bg-gray border border-border/20 text-gray-text '
 							}`}
 						>
-							<X className="mr-2" size={18} />
+							<div className="mr-1">
+								<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width={18} height={18}>
+									<path
+										fill="currentColor"
+										fillRule="evenodd"
+										d="M3.464 3.464C2 4.93 2 7.286 2 12s0 7.071 1.464 8.535C4.93 22 7.286 22 12 22s7.071 0 8.535-1.465C22 19.072 22 16.714 22 12s0-7.071-1.465-8.536C19.072 2 16.714 2 12 2S4.929 2 3.464 3.464m4.933 13.983a.75.75 0 0 0 1.05.155A4.27 4.27 0 0 1 12 16.75a4.27 4.27 0 0 1 2.553.852a.75.75 0 1 0 .894-1.204A5.77 5.77 0 0 0 12 15.25a5.77 5.77 0 0 0-3.447 1.148a.75.75 0 0 0-.156 1.049M16 10.5c0 .828-.448 1.5-1 1.5s-1-.672-1-1.5s.448-1.5 1-1.5s1 .672 1 1.5M9 12c.552 0 1-.672 1-1.5S9.552 9 9 9s-1 .672-1 1.5s.448 1.5 1 1.5"
+										clipRule="evenodd"
+									/>
+								</svg>
+							</div>
 							Unlucky
 						</button>
 					</div>
@@ -78,44 +88,54 @@ const TodayEntryForm: React.FC = () => {
 
 				{/* Description */}
 				<div className="mb-6">
-					<label className="block text-gray-300 text-sm font-medium mb-2">What happened?</label>
+					<label className="block text-gray-text  font-medium mb-2">What happened?</label>
 					<textarea
 						value={description}
 						onChange={(e) => setDescription(e.target.value)}
 						rows={3}
-						className="w-full bg-gray border border-border/20 focus:ring-0 outline-none rounded-lg p-3 text-gray-200 resize-none"
+						className="w-full bg-gray border text-white border-border/20 focus:ring-0 outline-none rounded-lg p-3 resize-none"
 						placeholder="Describe your lucky or unlucky event..."
 					/>
 				</div>
 
 				{/* Intensity */}
-
 				{type && (
-					<div className="mb-8">
-						<label className="block text-gray-300 text-sm font-medium mb-3">
-							How intense was it?
-						</label>
+					<div className="mb-6 flex items-center justify-center flex-col">
+						<label className=" text-gray-text font-medium  mb-2">How intense was it?</label>
 						<div className="flex gap-3">
 							{['normal', 'mid', 'super'].map((level, index) => {
 								const labels = {
-									lucky: ['Normal Lucky', 'Mid Lucky', 'Super Lucky'],
-									unlucky: ['Normal Unlucky', 'Mid Unlucky', 'Super Unlucky']
+									lucky: ['Normal', 'Mid', 'Super'],
+									unlucky: ['Normal', 'Mid', 'Super']
 								};
+
 								const isActive = intensity === index + 1;
+
+								const colorMap = {
+									lucky: [
+										'bg-green-500/20  border-green-500 text-green-300',
+										'bg-green-700/20  border-green-700 text-green-400',
+										'bg-green-900/20  border-green-900 text-green-500'
+									],
+									unlucky: [
+										'bg-orange-500/20 border-orange-500 text-orange-300',
+										'bg-orange-700/20 border-orange-700 text-orange-400',
+										'bg-orange-900/20 border-orange-900 text-orange-500'
+									]
+								};
+								const activeColor = colorMap[type][index];
 								return (
 									<button
 										key={level}
 										type="button"
 										onClick={() => setIntensity(index + 1)}
 										className={`px-4 py-2 rounded-lg border transition-all duration-200 text-sm font-medium
-              ${
-								isActive
-									? type === 'lucky'
-										? 'bg-green-500/20 border-green-500 text-white'
-										: 'bg-red-500/20 border-red-500 text-white'
-									: 'bg-gray-700/50 border-gray-600 text-gray-300 hover:bg-gray-700'
-							}
-            `}
+                        ${
+													isActive
+														? activeColor
+														: 'bg-gray border-border/20 text-gray-text'
+												}
+               `}
 									>
 										{labels[type][index]}
 									</button>
